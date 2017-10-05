f624cdde1fe5b5db75a67f0c4053ba03842f960b
PG-CHALAT\PG-Chalat <driftkao@gmail.com>
2017-10-05 14:00:10 +0700

    Add Account Class

--- OOP_CONCEPTS/src/MainClass/MainTest.java
+++ OOP_CONCEPTS/src/MainClass/MainTest.java
@@ -1,13 +1,11 @@
 package MainClass;
 
-import Week7.DemoWeek7;
-
-
 public class MainTest {
 	
 
 	public static void main(String[]args) throws Exception{
-		DemoWeek7.guessNumberGame();
+		
+				
 	}
 
 }
